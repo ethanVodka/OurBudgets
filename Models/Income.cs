6fe50134ba0499eb2926f30cc43c9f777de9f4e6
Ethan_Vodka <gohho1008@gmail.com>
2023-01-31 22:33:37 +0900

    re Initialize

---
+++ Models/Income.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurBudgets.Models
+{
+    public partial class Income
+    {
+        public enum IncomeType 
+        {
+            Undefine,
+            Payee,
+            Send
+        }
+
+        public IncomeType Kind { get; set; }
+        public int Value { get; set; }
+        public DateTime Date { get; set; }
+        public string Source { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurBudgets.Models
+{
+    /// <summary>
+    /// ユーザーモデル
+    /// </summary>
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public int UserAge { get; set; }
+        public string UserEmail { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime DayOfBirth { get; set; }
+    }
+}
